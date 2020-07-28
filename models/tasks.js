b8b75b1ed0ba32a8daff64d5220cab7f63eb2cb5
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-07-27 18:04:45 -0700

    refactored config and models directory to using sequelize init:config and sequelize init:models template

---
+++ models/tasks.js
@@ -0,0 +1,20 @@
+// tasks model
+
+module.exports = function (sequelize, DataTypes) {
+    var Tasks = sequelize.define("Tasks", {
+        startTime: DataTypes.STRING,
+        stopTime: DataTypes.STRING,
+        comments: DataTypes.STRING,
+        emoji: DataTypes.INTEGER
+    });
+
+    Tasks.associate = function(models) {
+        Tasks.belongsTo(models.Subgoals, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    return Tasks;
+};
